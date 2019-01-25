db3eac0a96bfa510606a0d1990253343cf941313
tmore <tmore@kth.se>
2019-01-25 20:45:42 +0100

    Done with interval cover

---
+++ cpp_lib/interval_cover.hpp
@@ -0,0 +1,86 @@
+// Authors: Tomas Möre, Marcus Östling
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <limits>
+#include <optional>
+
+namespace popup {
+
+  using range = std::pair<double, double>;
+
+  range make_range(double a, double b) { return std::make_pair(a,b); }
+
+
+  /**
+   * This function finds the indiceis for the minimum set of intervals that
+   * covers the specified range given by |range_left| and |range_right|.
+   * |ranges_start| and |ranges_end| should be some random access iterator over
+   * pairs or doubles representing each interval. The values of the range will
+   * not be modified.
+   * If no matching interval can be found it will return an empty vector.
+  */
+  template <class RandomAccessIterator>
+  std::optional<std::vector<size_t>>
+  interval_cover( double range_left,
+                  double range_right,
+                  RandomAccessIterator ranges_start,
+                  RandomAccessIterator ranges_end) {
+    std::vector<size_t> result;
+
+    // Keeps track of the original indices. This is the only mo
+    std::vector<size_t> indices(ranges_end - ranges_start);
+    for (size_t i = 0; i < indices.size(); i++) {
+      indices[i] = i;
+    }
+
+    auto start = indices.begin();
+    auto end = indices.end();
+
+    // Comparison method for the intervals. First by left value and secondarily
+    // by right.
+    auto comp = [&](const size_t &a_idx, const size_t &b_idx) {
+                  const range& a = *(ranges_start + a_idx);
+                  const range& b = *(ranges_start + b_idx);
+
+                  if (a.first == b.first) {
+                    return a.second > b.second;
+                  } else {
+                    return a.first < b.first;
+                  }
+                };
+
+    std::sort(start, end, comp);
+
+    double left = range_left;
+    auto current = start;
+
+    // Do because if left == right it still can find an interval.
+    do {
+      range best_range = make_range(-std::numeric_limits<double>::infinity(),
+                                    -std::numeric_limits<double>::infinity()
+                                    );
+      bool best_found = false;
+      size_t best_idx = 0;
+
+      // Finds the
+      for (; current != end && (ranges_start + (*current))->first <= left; current++) {
+        auto r = *(ranges_start + (*current));
+        if (r.second > best_range.second) {
+          best_range = r;
+          best_found = true;
+          best_idx = *current;
+        }
+      }
+      if (best_found) {
+        result.push_back(best_idx);
+        left = best_range.second;
+      } else {
+        //
+        return std::nullopt;
+      }
+    } while (left < range_right);
+
+    return std::make_optional(result);
+  }
+}
--- kattis/interval_cover/main.cpp
+++ kattis/interval_cover/main.cpp
@@ -5,27 +5,33 @@
 int main() {
   double left, right;
   size_t num_ranges;
-  std::vector<popup::range> ranges;
 
-  std::cin >> left >> right >> num_ranges;
+  while (std::cin >> left >> right >> num_ranges) {
+    std::vector<popup::range> ranges;
 
-  for (int i = 0; i < num_ranges; i++) {
-    double a, b;
-    std::cin >> a >> b;
-    // Experiment with push_back / emplace_back
-    ranges.emplace_back(popup::make_range(a, b));
-  }
-  auto res = popup::interval_cover(popup::make_range(left,right),
-                                   ranges.begin(),
-                                   ranges.end());
-  if (res.empty()) {
-    std::cout << "impossibe\n";
-  } else {
-    std::cout << res.size() << std::endl;
-    for (auto i : res) {
-      std::cout << i;
+    // std::cerr << left << " " << right << " " << num_ranges << std::endl;
+    for (int i = 0; i < num_ranges; i++) {
+      double a, b;
+      std::cin >> a >> b;
+      // Experiment with push_back / emplace_back
+      //      std::cerr << a <<":"<< b << std::endl;
+      ranges.emplace_back(popup::make_range(a, b));
     }
+
+    auto res = popup::interval_cover(left,
+                                     right,
+                                     ranges.begin(),
+                                     ranges.end());
+    if (!res.has_value()) {
+      std::cout << "impossible\n";
+    } else {
+      auto vec = res.value();
+      std::cout << vec.size() << std::endl;
+      for (auto i : vec) {
+        std::cout << i << " ";
+      }
+      std::cout << std::endl;
+    }
+
   }
-
-
 }
