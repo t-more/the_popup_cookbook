39785a91218f872fbfd402e51d4f6433342667b1
tmore <tmore@kth.se>
2019-02-05 22:28:56 +0100

    Added wroking solution

--- cpp_lib/partial_linear_equation_sovler.hpp
+++ cpp_lib/partial_linear_equation_sovler.hpp
@@ -1,3 +1,4 @@
+// Authors: Tomas Möre, Marcus Östring
 #include <iostream>
 #include <vector>
 #include <cmath>
@@ -9,14 +10,15 @@
 #define EPS 1e-12
 
 namespace popup {
-  enum NUM_SOLUTIONS {
-                      NONE = 0,
-                      ONE = 1,
-                      MULTIPLE = 2
+
+  enum SOLTUION_TYPE {
+                 NONE = 0,
+                 ONE = 1,
+                 MULTIPLE = 2
   };
 
   template<typename T>
-  int linear_solve(std::vector<std::vector<T>>& a,
+  SOLTUION_TYPE linear_solve(std::vector<std::vector<T>>& a,
                    std::vector<T>& b,
                    std::vector<T>& ans
                    ) {
@@ -25,13 +27,10 @@ namespace popup {
     const auto num_columns = a[0].size();
 
     std::vector<int> is_used(num_columns, -1);
-    std::vector<int> cols(num_columns);
-    std::iota(cols.begin(), cols.end(), 0);
 
     size_t pivot_row, pivot_column; // h, k
     pivot_row = pivot_column = 0;
     int rank = 0;
-
 
     while (pivot_row < num_rows && pivot_column < num_columns) {
       size_t row_max = pivot_row;
@@ -45,30 +44,29 @@ namespace popup {
         }
       }
 
-      if (a[row_max][pivot_column] < EPS) {
+      if (std::fabs(a[row_max][pivot_column]) < EPS) {
         pivot_column++;
         continue;
-      } else {
-        std::swap(a[pivot_row], a[row_max]);
-        std::swap(b[pivot_row], b[row_max]);
-        std::swap(cols[pivot_row], cols[row_max]);
-
-        for (int i = pivot_row + 1; i < num_rows; i++) {
-          const auto factor = a[i][pivot_column] / a[pivot_row][pivot_column];
-          a[i][pivot_column] = 0;
-
-          for (int j = pivot_column + 1; j < num_columns; j++) {
-            a[i][j] -= factor * a[pivot_row][j];
-          }
-          b[i] -= factor * b[pivot_row];
-        }
-
-
-        is_used[pivot_column] = pivot_row;
-
-        pivot_row++;
-        pivot_column++;
       }
+
+      std::swap(a[pivot_row], a[row_max]);
+      std::swap(b[pivot_row], b[row_max]);
+
+      for (int i = pivot_row + 1; i < num_rows; i++) {
+        const auto factor = a[i][pivot_column] / a[pivot_row][pivot_column];
+        a[i][pivot_column] = 0;
+
+        for (int j = pivot_column + 1; j < num_columns; j++) {
+          a[i][j] -= factor * a[pivot_row][j];
+        }
+        b[i] -= factor * b[pivot_row];
+      }
+
+      is_used[pivot_column] = pivot_row;
+
+      pivot_row++;
+      pivot_column++;
+
       rank++;
     }
 
@@ -79,31 +77,37 @@ namespace popup {
     //   std::cout << "\n";
     // }
 
+    // for (auto& inner : b) {
+    //   std::cout << inner << " ";
+    // }
+    // std::cout << "\n";
+    // std::cout << "Rank: " << rank << "\n";
+    for (int row = num_rows-1; row >= rank; row--) {
+      T sum = 0;
+      for (int i = 0; i < num_columns; i++)
+        sum += a[row][i];
+      //      std::cout << "Sum: " << sum << "\n";
+      if (std::fabs(sum - b[row]) > EPS) {
+        return NONE;
+      }
+    }
+
     ans.assign(rank, 0);
-
     // Corrects the b values as it goes to correct
-    for (int i = rank -1; i >= 0; i--) {
-      ans[cols[i]] = b[i];
-      b[i] /= a[i][i];
-      for (int j = i - 1; j >= 0; j--) {
-        b[j] -= a[j][i] * b[i];
+    for (int row = rank -1; row >= 0; row--) {
+      b[row] /= a[row][row];
+      ans[row] = b[row];
+      for (int j = row - 1; j >= 0; j--) {
+        b[j] -= a[j][row] * b[row];
       }
     }
 
-
-    for (int i = 0; i < num_rows; i++) {
-      double sum = 0;
-      for (int j = 0; j < num_columns; j++) {
-        sum += ans[cols[j]] * a[i][j];
-      }
-
-      // If the sum of the row and b is above EPS then. There are no solution.
-      if (std::abs(sum - b[i]) > EPS) {
-        std::cout << "POTATO: " << std::abs(sum - b[i]) << std::endl;
-        return 0;
-      }
+    if (rank < num_rows) {
+      return MULTIPLE;
+    } else {
+      return ONE;
     }
-
-    return (-(rank == num_rows)) + 2;
   }
+
+
 }
