1e30b0fc699488c514fb4e8f69a57f59ebe496a4
tmore <tmore@kth.se>
2019-02-05 20:51:22 +0100

    Almost done with solver

--- cpp_lib/partial_linear_equation_sovler.hpp
+++ cpp_lib/partial_linear_equation_sovler.hpp
@@ -1,51 +1,109 @@
+#include <iostream>
 #include <vector>
 #include <cmath>
 #include <utility>
 #include <limits>
+#include <algorithm>
+#include <numeric>
+
+#define EPS 1e-12
+
+namespace popup {
+  enum NUM_SOLUTIONS {
+                      NONE = 0,
+                      ONE = 1,
+                      MULTIPLE = 2
+  };
+
+  template<typename T>
+  int linear_solve(std::vector<std::vector<T>>& a,
+                   std::vector<T>& b,
+                   std::vector<T>& ans
+                   ) {
+
+    const auto num_rows = a.size();
+    const auto num_columns = a[0].size();
+
+    std::vector<int> is_used(num_columns, -1);
+    std::vector<int> cols(num_columns);
+    std::iota(cols.begin(), cols.end(), 0);
+
+    size_t pivot_row, pivot_column; // h, k
+    pivot_row = pivot_column = 0;
+    int rank = 0;
 
 
+    while (pivot_row < num_rows && pivot_column < num_columns) {
+      size_t row_max = pivot_row;
+      T max_found = std::fabs(a[pivot_row][pivot_column]);
 
-
-
-
-template<typename T>
-void linear_solve(std::vector<std::vector<T>>& a, std::vector<T>& b) {
-  const auto num_rows = a.size();
-  const auto num_columns = a[0].size();
-
-  size_t pivot_row, pivot_column; // h, k
-  pivot_row = pivot_column = 1;
-  int rank = 0;
-  while (pivot_row <= num_rows && pivot_column <= num_columns) {
-    size_t row_max = 0;
-    T max_found = -1;
-    for (size_t i = pivot_row; i < num_rows; i++) {
-      auto value = std::fabs(a[i][pivot_column]);
-      if (value > max_found) {
-        row_max = i;
-        max_found = value;
-      }
-    }
-
-    if (a[row_max][pivot_column] == 0) {
-      pivot_column++;
-      continue;
-    } else {
-      std::swap(a.begin() + pivot_row, a.begin() + row_max);
-      std::swap(b.begin() + pivot_row, b.begin() + row_max);
-      for (int i = pivot_row; i < num_rows; i++) {
-        const auto factor = a[i][pivot_column] / a[pivot_row][pivot_column];
-
-        a[i][pivot_column] = 0;
-
-        for (int j = pivot_column; j < num_columns; j++) {
-          a[i][j] -= a[pivot_row][j] * factor;
+      for (size_t i = pivot_row + 1; i < num_rows; i++) {
+        auto value = std::fabs(a[i][pivot_column]);
+        if (value > max_found) {
+          row_max = i;
+          max_found = value;
         }
       }
-      pivot_row++;
-      pivot_column++;
-    }
-    rank++;
-  }
 
+      if (a[row_max][pivot_column] < EPS) {
+        pivot_column++;
+        continue;
+      } else {
+        std::swap(a[pivot_row], a[row_max]);
+        std::swap(b[pivot_row], b[row_max]);
+        std::swap(cols[pivot_row], cols[row_max]);
+
+        for (int i = pivot_row + 1; i < num_rows; i++) {
+          const auto factor = a[i][pivot_column] / a[pivot_row][pivot_column];
+          a[i][pivot_column] = 0;
+
+          for (int j = pivot_column + 1; j < num_columns; j++) {
+            a[i][j] -= factor * a[pivot_row][j];
+          }
+          b[i] -= factor * b[pivot_row];
+        }
+
+
+        is_used[pivot_column] = pivot_row;
+
+        pivot_row++;
+        pivot_column++;
+      }
+      rank++;
+    }
+
+    // for (auto& outer : a) {
+    //   for (auto& inner : outer) {
+    //     std::cout << inner << " ";
+    //   }
+    //   std::cout << "\n";
+    // }
+
+    ans.assign(rank, 0);
+
+    // Corrects the b values as it goes to correct
+    for (int i = rank -1; i >= 0; i--) {
+      ans[cols[i]] = b[i];
+      b[i] /= a[i][i];
+      for (int j = i - 1; j >= 0; j--) {
+        b[j] -= a[j][i] * b[i];
+      }
+    }
+
+
+    for (int i = 0; i < num_rows; i++) {
+      double sum = 0;
+      for (int j = 0; j < num_columns; j++) {
+        sum += ans[cols[j]] * a[i][j];
+      }
+
+      // If the sum of the row and b is above EPS then. There are no solution.
+      if (std::abs(sum - b[i]) > EPS) {
+        std::cout << "POTATO: " << std::abs(sum - b[i]) << std::endl;
+        return 0;
+      }
+    }
+
+    return (-(rank == num_rows)) + 2;
+  }
 }
