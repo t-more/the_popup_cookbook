62e7b73d31ce6de868535e00bdb2c4ab22f5ddda
tmore <tmore@kth.se>
2019-03-25 21:10:56 +0100

    Mackes crack

--- cpp_lib/math/modular.hpp
+++ cpp_lib/math/modular.hpp
@@ -2,56 +2,70 @@
 #include <tuple>
 #include <optional>
 
+
 namespace popup {
 
-template <typename T, typename N>
-std::tuple<T, N, N> extended_euclidean(T a, T b) {
-    if(a == 0) {
-        return {b, 0, 1};
+    template <typename T, typename N>
+    std::tuple<T, N, N> extended_euclidean(T a, T b) {
+        if(a == 0) {
+            return {b, 0, 1};
+        }
+
+        auto [gcd, x, y] = extended_euclidean<T, N>(b % a, a);
+
+        return {gcd, y - (b/a) * x, x};
     }
 
-    auto [gcd, x, y] = extended_euclidean<T, N>(b % a, a);
+    template <typename T, typename N>
+    std::optional<T> mod_inverse(T a, T m) {
+        auto [gcd, x, y] = extended_euclidean<T, N>(a, m);
 
-    return {gcd, y - (b/a) * x, x};
-}
+        if(gcd != 1) {
+            return std::nullopt;
+        }
 
-template <typename T, typename N>
-std::optional<T> mod_inverse(T a, T m) {
-    auto [gcd, x, y] = extended_euclidean<T, N>(a, m);
+        return (x % (N)m + (N)m) % m;
+    }
 
-    if(gcd != 1) {
+    template <typename T>
+    T mul_mod(T a, T b, T m) {
+        return (a*b)%m;
+    }
+
+    template <typename T, typename N>
+    std::optional<T> div_mod(T a, T b, T m) {
+        a %= m;
+        auto inv = mod_inverse<T, N>(b, m);
+        if(inv.has_value()) {
+            return (inv.value() * a) % m;
+        }
+        return inv;
+    }
+
+    template <typename T>
+    T add_mod(T a, T b, T m) {
+        return (a+b)%m;
+    }
+
+    template <typename T>
+    T sub_mod(T a, T b, T m) {
+        a %= m;
+        b %= m;
+        if(b > a) a += m;
+        return (a-b)%m;
+    }
+
+    template<typename T>
+    std::optional<T> chinese_rest_theorem(T a1, T m1, T a2, T m2) {
+        auto [gcd, n1, n2] = extended_euclidean<T, int64_t>(m1, m2);
+        if (gcd == 1) {
+            // Specia case of two acording to wikipedia
+            T ans = ((a1 * (n2 + m1)) % m1) * m2 + ((a2 * (n1 + m2)) % m2) * m1;
+            if (ans >= m1 * m2)
+                ans -= m1 * m2;
+            return ans;
+        }
         return std::nullopt;
     }
-
-    return (x % (N)m + (N)m) % m;
-}
-
-template <typename T>
-T mul_mod(T a, T b, T m) {   
-    return (a*b)%m;
-}
-
-template <typename T, typename N>
-std::optional<T> div_mod(T a, T b, T m) {
-    a %= m;
-    auto inv = mod_inverse<T, N>(b, m);
-    if(inv.has_value()) {
-        return (inv.value() * a) % m;
-    }
-    return inv;
-}
-
-template <typename T>
-T add_mod(T a, T b, T m) {
-    return (a+b)%m;
-}
-
-template <typename T>
-T sub_mod(T a, T b, T m) {
-    a %= m;
-    b %= m;
-    if(b > a) a += m;
-    return (a-b)%m;
-}
 
 } // namespace popup
