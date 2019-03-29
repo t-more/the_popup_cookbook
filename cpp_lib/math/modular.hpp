bac4909b5fd81facb3bc22e93c388de5c12303b1
Marcus Östling <mpt.ostling@gmail.com>
2019-03-29 13:13:22 +0100

    general CRT

--- cpp_lib/math/modular.hpp
+++ cpp_lib/math/modular.hpp
@@ -2,7 +2,6 @@
 #include <tuple>
 #include <optional>
 
-
 namespace popup {
 
     template <typename T, typename N>
@@ -55,6 +54,9 @@ namespace popup {
         return (a-b)%m;
     }
 
+    /**
+     *  Chinese rest theorem where m1 and m2 are co-prime.
+     */
     template<typename T>
     std::optional<T> chinese_rest_theorem(T a1, T m1, T a2, T m2) {
         auto [gcd, n1, n2] = extended_euclidean<T, int64_t>(m1, m2);
@@ -64,6 +66,22 @@ namespace popup {
             if (ans >= m1 * m2)
                 ans -= m1 * m2;
             return ans;
+        }
+        return std::nullopt;
+    }
+
+    /**
+     *  General case of chinese rest theorem where
+     *  m1 and m2 doesn't have to be co-prime.
+     *  https://en.wikipedia.org/wiki/Chinese_remainder_theorem
+     */
+    template<typename T>
+    std::optional<T> general_chinese_rest_theorem(T a1, T m1, T a2, T m2) {
+        auto [gcd, n1, n2] = extended_euclidean<T, int64_t>(m1, m2);
+        if(((a1 + gcd) % gcd) == (a2 + gcd) % gcd) { // unique solution
+            T lcm = (m1*m2)/gcd;
+            T ans = ((a1 * (n2 + m1)) % m1) * m2 + ((a2 * (n1 + m2)) % m2) * m1;
+            return (ans/gcd) % lcm;
         }
         return std::nullopt;
     }
