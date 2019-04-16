c3c51f8df0c7dd4875f4ed73912a297d4e552fb1
Marcus Östling <mpt.ostling@gmail.com>
2019-04-16 14:23:43 +0200

    ba

--- kattis/hashing/main.cpp
+++ kattis/hashing/main.cpp
@@ -15,18 +15,37 @@ unsigned long long mod(unsigned long long x, unsigned long long m) {
     return ((x % m) + m) % m;
 }
 
+int64_t mul_mod(uint64_t a, uint64_t b, uint64_t m)
+{
+   uint64_t d = 0, mp2 = m >> 1;
+   int i;
+   if (a >= m) a %= m;
+   if (b >= m) b %= m;
+   for (i = 0; i < 64; ++i)
+   {
+       d = (d > mp2) ? (d << 1) - m : d << 1;
+       if (a & 0x8000000000000000ULL)
+           d += b;
+       if (d >= m) d -= m;
+       a <<= 1;
+   }
+   return d;
+}
+
 void compute_hash(
         string const& s, 
         vector<unsigned long long> &hashes,
         unsigned long long m,
         unsigned long long p)
 {
-    long long hash_value = 0;
-    long long p_pow = 1;
+    unsigned long long hash_value = 0;
+    unsigned long long p_pow = 1;
     for (char c : s) {
-        hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
+        //hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
+        hash_value = (hash_value + mul_mod(c - 'a' + 1, p_pow, m)) % m;
         hashes.push_back(hash_value);
-        p_pow = (p_pow * p) % m;
+        //p_pow = (p_pow * p) % m;
+        p_pow = mul_mod(p_pow, p, m);
 
     }
 }
@@ -46,23 +65,23 @@ int main() {
     vector<unsigned long long> inv(str.size());
     inv[0] = 1;
     inv[1] = 838709685; // pow(2, 1e9+9) % m
-    for(unsigned long long i = 2; i <= p; i++) {
-        inv[i] = (inv[i-1] * inv[1]) % m;
+    for(unsigned long long i = 2; i < str.size(); i++) {
+        //inv[i] = (inv[i-1] * inv[1]) % m;
+        inv[i] = mul_mod(inv[i-1], inv[1], m);
     }
 
     vector<unsigned long long> hashes;
     compute_hash(str, hashes, m, p);
-    for(auto i : hashes) {
-        cerr << i << endl;
-    }
     for(int cnt = 0; cnt < q; cnt++) {
         int i, j;
         cin >> i >> j;
+        j--;
         if(i == 0) {
-            cerr << "i == 0\n";
-            cout << hashes[j-1] << "\n";
+            cout << hashes[j] << "\n";
         } else {
-            cout << mod(mod(hashes[j-1] - hashes[i-2], m)*inv[i-1], m) << "\n";
+            i--;
+            //cout << mod(mod(hashes[j] - hashes[i], m)*inv[i+1], m) << "\n";
+            cout << mul_mod(mod(hashes[j] - hashes[i], m), inv[i+1], m) << "\n";
         }
     }
 
